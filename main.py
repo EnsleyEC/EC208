0b3ab6b51152de42704a9a07605fc0cd9be5162f
Ensley Ribeiro <30347664+EnsleyEC@users.noreply.github.com>
2017-11-19 20:47:44 -0200

    Projeto Memoria Cache

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+# encoding: utf-8
+
+''' valores padrao para soma e subtracao '''
 op_soma = 1
 op_sub = 2
+
+def inicializa():
+	nome_arquivo = 'ensley.txt'
+	write_arquivo(nome_arquivo)
+	tag = [' ',' ',' ',' ',' ',' ',' ',' ']
+	validade = [0,0,0,0,0,0,0,0]
+	pc = 0
+
+	return nome_arquivo, tag, validade, pc
+
+def binario_para_decimal(valor_binario, tamanho_valor):
+
+	valor_binario_str = str(valor_binario)
+	valor_decimal = 0
+	cont = tamanho_valor - 1
+
+	# range(0,5) - 0 ate 4
+	for i in range(0,tamanho_valor):
+		if(valor_binario_str[i] == '1'):
+			valor_decimal = valor_decimal + pow(2,cont)
+		cont = cont - 1
+
+	return valor_decimal
+
+def decimal_para_binario(n):
+    '''funcao para converter decimal em binario'''
+
+    binario = ""
+    while(True):
+        binario = binario + str(n%2)
+        n = n//2
+        if n == 0:
+            break
+    binario = binario[::-1]
+    binario = int(binario)
+    return binario
 
 def readArquivo(pc):
 	'''lendo a instrucao do pc'''
@@ -22,28 +61,94 @@ def obter_instrucao(instrucao):
 
 	return soma
 
-def obter_dados(instrucao):
+def write_cache(novo_dado,posicao_para_atualizar_dado):
+	'''Se deu miss, escrevo o novo dado na cache'''
+	dados = open('cache.txt','w')
+	dados_cache = dados.readlines()
+
+	for linha in dados_cache:
+		if linha is dados_cache[posicao_para_atualizar_dado]:
+			dados.write(str(novo_dado)+'\n')
+		else:
+			dados.write(linha)
+
+	novo_fila.close()
+
+def cache(registro,tag,validade):
+
+	m_cache = open('cache.txt','r')
+	dados = m_cache.readlines()
+	peguei_valor_da_cache = False
+	valor_da_cache = 0
+	registro_tag = registro[3]+registro[4]
+	'''
+	Exemplo:
+	Registro = 111-00
+
+	111 = posição da cache de 0 a 8
+	00 = TAG
+	'''
+
+	# pegando a posição da cache
+	posicao_cache = binario_para_decimal(registro,3)
+
+	'''
+	HIT, se:
+	* validade[posicao_cache] == 1
+	* tag[posicao_cache] == tag do registro
+
+	se nao:
+	* miss
+
+	se for miss:
+	* validade[posicao_cache] = 1
+	* tag[posicao_cache] = tag do registro
+	* escrever o dado na memoria cache
+	'''
+
+	if validade[posicao_cache] == 1:
+
+		if registro_tag is tag[posicao_cache]:
+			peguei_valor_da_cache = True
+			valor_da_cache = int(dados[posicao_cache])
+			print("Hit")
+		else:
+			tag[posicao_cache] = registro_tag
+			peguei_valor_da_cache = False
+			print("Miss")
+	else:
+		validade[posicao_cache] = 1
+		tag[posicao_cache] = registro_tag
+		peguei_valor_da_cache = False
+		print("Miss")
+
+	return tag,validade,valor_da_cache,peguei_valor_da_cache
+
+def registros(instrucao):
 	'''Pegando o primeiro e o segundo valor da memoria'''
-	cont = 4
-	rs = rt = rd = 0
+
+	rs = [' ',' ',' ',' ',' ']
+	rt = [' ',' ',' ',' ',' ']
+	rd = [' ',' ',' ',' ',' ']
+	j = 0
+
+	print('Type rs = ',type(rs))
 
 	for i in range(6,11):
-		if(instrucao[i] == '1'):
-			rs = rs + pow(2,cont)
-		cont = cont - 1
-	cont = 4
+		rs[j] = instrucao[i]
+		j = j + 1
+
+	j = 0
 
 	for i in range(11,16):
-		if(instrucao[i] == '1'):
-			rt = rt + pow(2,cont)
-		cont = cont - 1
+		rt[j] = instrucao[i]
+		j = j + 1
 
-	cont = 4
+	j = 0
+
 	for i in range(16,21):
-		if(instrucao[i] == '1'):
-			rd = rd + pow(2,cont)
-		cont = cont - 1
-
+		rd[j] = instrucao[i]
+		j = j + 1
 
 	return rs,rt,rd
 
@@ -71,73 +176,109 @@ def executar(rs,rt,funct):
 
 	return resultado
 
-def buscar_memoria(rs,rt):
+def load(rs,rt,tag,validade):
 
-	''' pegando o valor em binario contigo em rt e rs, e passando pra decimal'''
+	print('RS = ',rs)
+	print('RT = ',rt)
+
+	'''
+	A = true ----- RS existe na cache
+	B = true ----- RT existe na cache
+	'''
+	tag, validade , rs_cache, a = cache(rs,tag,validade)
+	tag, validade , rt_cache, b = cache(rt,tag,validade)
 
 	m_dados = open('dados.txt','r')
-	dados = m_dados.readlines()
+	dados_memoria_principal = m_dados.readlines()
 
-	_rs = dados[rs-1]
-	_rt = dados[rt-1]
+	if a == True and b == True:
 
-	rs_decimal = 0
-	rt_decimal = 0
+		rs_decimal = binario_para_decimal(rs_cache,len(rs_cache))
+		rt_decimal = binario_para_decimal(rt_cache,len(rt_cache))
 
-	cont = 31
+		if rs_cache[0] is '1':
+			rs_decimal = rs_decimal*(-1)
+		if rt_cache[0] is '1':
+			rt_decimal = rt_decimal*(-1)
 
-	for i in range(1,len(_rs)):
-		if(_rs[i] == '1'):
-			rs_decimal = rs_decimal + pow(2,cont)
-		cont = cont - 1
+		return rs_decimal,rt_decimal,tag,validade
 
-	cont = 31
+	elif a == True:
 
-	for i in range(1,len(_rt)):
-		if(_rt[i] == '1'):
-			rt_decimal = rt_decimal + pow(2,cont)
-		cont = cont - 1
+		rs_decimal = binario_para_decimal(rs_cache,len(rs_cache))
 
-	m_dados.close()
+		rt_pos_memoria_principal = binario_para_decimal(rt,len(rt))
+		rt_binario = dados_memoria_principal[rt_pos_memoria_principal]
+		rt_decimal = binario_para_decimal(rt_binario,len(rt_binario))
 
-	if _rs[0] is '1':
-		rs_decimal = rs_decimal*(-1)
-	if _rt[0] is '1':
-		rt_decimal = rt_decimal*(-1)	
+		m_dados.close()
 
-	return rs_decimal,rt_decimal
+		if rs_cache[0] is '1':
+			rs_decimal = rs_decimal*(-1)
+		if rt_binario[0] is '1':
+			rt_decimal = rt_decimal*(-1)
 
-def converterd_b(n):
-    '''funcao para converter decimal em binario'''
+		return rs_decimal,rt_decimal,tag,validade
 
-    binario = ""
-    while(True):
-        binario = binario + str(n%2)
-        n = n//2
-        if n == 0:
-            break
-    binario = binario[::-1]
-    binario = int(binario)
-    return binario
 
-def salvar_dados(resultado,rd,sinal):
+
+	elif b == True:
+
+		rt_decimal = binario_para_decimal(rt_cache,len(rt_cache))
+
+		rs_pos_memoria_principal = binario_para_decimal(rs,len(rs))
+		rs_binario = dados_memoria_principal[rs_pos_memoria_principal]
+		rs_decimal = binario_para_decimal(rs_binario,len(rs_binario))
+
+		m_dados.close()
+
+		if rs_binario[0] is '1':
+			rs_decimal = rs_decimal*(-1)
+		if rt_cache[0] is '1':
+			rt_decimal = rt_decimal*(-1)
+
+		return rs_decimal,rt_decimal,tag,validade
+
+	else:
+
+		rt_pos_memoria_principal = binario_para_decimal(rt,len(rt))
+		print('Pos RT = ',rt_pos_memoria_principal)
+		rt_binario = dados_memoria_principal[rt_pos_memoria_principal]
+		rt_decimal = binario_para_decimal(rt_binario,len(rt_binario))
+
+		rs_pos_memoria_principal = binario_para_decimal(rs,len(rs))
+		rs_binario = dados_memoria_principal[rs_pos_memoria_principal]
+		rs_decimal = binario_para_decimal(rs_binario,len(rs_binario))
+
+		m_dados.close()
+
+		if rs_binario[0] is '1':
+			rs_decimal = rs_decimal*(-1)
+		if rt_binario[0] is '1':
+			rt_decimal = rt_decimal*(-1)
+
+		return rs_decimal,rt_decimal,tag,validade
+
+def store(resultado,rd,sinal):
 	'''funcao que salva os dados na memoria de dados'''
 
 	text_arquivo = open('dados.txt','r')
 	texto = text_arquivo.readlines()
 	escrever = open('dados.txt','w')
+	rd = binario_para_decimal(rd,len(rd))
 
 	for linha in texto:
 
-		if linha is texto[rd-1]:
-			resp = converterd_b(resultado)
+		'''converter rd '''
+		if linha is texto[rd]:
+			resp = decimal_para_binario(resultado)
 
 			tam_resp = len(str(resp))
 
 			if sinal is 0:
 				escrever.write(str(sinal))
 			else:
-				escrever.write(str(sinal))
+		 		escrever.write(str(sinal))
 
 			for i in range(2,34-tam_resp):
 					escrever.write('0')
@@ -150,7 +291,7 @@ def salvar_dados(resultado,rd,sinal):
 	escrever.close()
 
 def tamanho_pc(nome_arquivo):
-
+	''' Função que retorna o numero de instrucoes na memoria principal '''
 	with open(nome_arquivo,'r') as line:
 		tamanho = len(line.readlines())
 		line.close()
@@ -159,25 +300,31 @@ def tamanho_pc(nome_arquivo):
 def main():
 	'''funcao principal'''
 
-	nome_arquivo = 'ensley.txt'
-
-	write_arquivo(nome_arquivo)
-
+	nome_arquivo, tag , validade, pc = inicializa()
 	pc_max = tamanho_pc(nome_arquivo)
 
-	pc = 0;
-
 	while pc != pc_max:
+
+		# instru é a instrucao completa da memoria RAM de 32 bits
 		instru = readArquivo(pc)
+		# da instrucao de 32 bits, pegando qual operação será realizada(add,sub...)
 		instrucao = obter_instrucao(instru)
-		rs,rt,rd = obter_dados(instru)
-		_rs,_rt = buscar_memoria(rs,rt)
+		'''
+		Exemplo: C = A + B
+		RS = registro do A
+		RT = registro do B
+		RD = registro do C
+		'''
+		rs,rt,rd = registros(instru)
+		# buscando os valores de contidos nos registradores RS e RT
+		_rs,_rt,tag,validade = load(rs,rt,tag,validade)
+		# com os valores de A e B, executo a instrução e retorno a resposta
 		resultado = executar(_rs,_rt,instrucao)
 		# 1 - negativo, 0 - positivo
 		if resultado >= 0:
-			salvar_dados(resultado,rd,0)
+			store(resultado,rd,0)
 		else:
-			salvar_dados(abs(resultado),rd,1)
+			store(abs(resultado),rd,1)
 		pc = pc + 1
 
 if __name__ == '__main__':
